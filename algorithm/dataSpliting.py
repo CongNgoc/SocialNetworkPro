0127a6b94d699d24b16e150282e186c08940ef3d
CongNgoc <ngoccong11a3@gmail.com>
2020-06-11 17:47:11 +0700

    add specify_clustering is kMeans algorithm with string value

--- algorithm/dataSpliting.py
+++ algorithm/dataSpliting.py
@@ -6,24 +6,13 @@ from sklearn.model_selection import train_test_split
 file_questions = '../resources/result_prep_questions.csv'
 file_answers = '../resources/result_prep_answers.csv'
 
-df_questions = dp.read_csv(file_questions)
-df_answers = dp.read_csv(file_answers)
+def splitDataset(file_ques = file_questions, file_ans = file_answers): 
+	df_questions = dp.read_csv(file_questions)
+	df_answers = dp.read_csv(file_answers)
 
-# Dataset splitting
-questions_train_set, questions_test_set = train_test_split(df_questions, train_size=0.7, random_state=42)
-ansers_train_set, ansers_test_set = train_test_split(df_answers, train_size=0.5, random_state=42)
-# # Print Data
-# print(X)
-# print(y)
-print(questions_train_set['Id'][[5191]])
-
-# Get item from dataframe
-print(type(questions_train_set))
-print("=========================================================")
-# print(ansers_train_set)
-# print(X_test)
-# print(y_train)
-# print(y_test)
-# print(tokens_without_sw)
+	# Dataset splitting
+	questions_train_set, questions_test_set = train_test_split(df_questions, train_size=0.7, random_state=42)
+	ansers_train_set, ansers_test_set = train_test_split(df_answers, train_size=0.5, random_state=42)
+	return questions_train_set, questions_test_set, ansers_train_set, ansers_test_set
 
 
--- algorithm/kMeanAlgorithm.py
+++ algorithm/kMeanAlgorithm.py
@@ -4,15 +4,17 @@ import matplotlib.pyplot as plt
 # %matplotlib inline 
 from sklearn.cluster import KMeans
 from sklearn import datasets
+from dataSpliting import splitDataset
 
 
 # DATASET
 iris = datasets.load_iris()
+# questions_train_set, questions_test_set, ansers_train_set, ansers_test_set = splitDataset()
 
 #we are usingh
+# df=pd.DataFrame(questions_train_set)
 df=pd.DataFrame(iris['data'])
-
-print(df)
+# print(df)
 
 
 # Runing KMeans with a range of k
@@ -20,12 +22,12 @@ distortions = []
 K = range(1,10)
 for k in K:
     kmeanModel = KMeans(n_clusters=k)
-    kmeanModel.fit(df)
+    kmeanModel.fit(iris['data'])
     distortions.append(kmeanModel.inertia_)
     print(kmeanModel.inertia_)
 
 print(distortions)    
-
+print("=======Gia tri cua K %d" %K)
 
 # Plotting the distortions of K-Means
 
@@ -38,13 +40,13 @@ plt.title('The Elbow Method showing the optimal k')
 
 
 kmeanModel = KMeans(n_clusters=3)
-kmeanModel.fit(df)
+kmeanModel.fit(iris['data'])
 
-df['k_means']=kmeanModel.predict(df)
+df['k_means']=kmeanModel.predict(iris['data'])
 df['target']=iris['target']
 fig, axes = plt.subplots(1, 2, figsize=(16,8))
 axes[0].scatter(df[0], df[1], c=df['target'])
 axes[1].scatter(df[0], df[1], c=df['k_means'], cmap=plt.cm.Set1)
 axes[0].set_title('Actual', fontsize=18)
 axes[1].set_title('K_Means', fontsize=18)
-plt.show()
+# plt.show()
